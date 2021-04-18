26d8b53ae1139edcf5d0d557879e5ef8c0a951b4
OlegMilkin <olegek.milkin@gmail.com>
2021-04-18 23:50:29 +0300

    home work

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {Col, Row, Container} from 'reactstrap';
 import Header from '../header';
 import RandomChar from '../randomChar';
@@ -6,29 +6,48 @@ import ItemList from '../itemList';
 import CharDetails from '../charDetails';
 
 
-const App = () => {
-    return (
-        <> 
-            <Container>
-                <Header />
-            </Container>
-            <Container>
-                <Row>
-                    <Col lg={{size: 5, offset: 0}}>
-                        <RandomChar/>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col md='6'>
-                        <ItemList />
-                    </Col>
-                    <Col md='6'>
-                        <CharDetails />
-                    </Col>
-                </Row>
-            </Container>
-        </>
-    );
-};
+export default class App extends Component {
 
-export default App;
+    constructor() {
+        super()
+        this.state = {
+            isRandom: true
+        }
+
+        this.randomToogle = this.randomToogle.bind(this)
+    }
+    
+
+    randomToogle() {
+        this.setState({
+            isRandom: !this.state.isRandom,
+        })
+    }
+
+    render() {
+        const randomShow = this.state.isRandom ? <RandomChar/> : null;
+        return(
+            <> 
+                <Container>
+                    <Header />
+                </Container>
+                <Container>
+                    <Row>
+                        <Col lg={{size: 5, offset: 0}}>
+                            {randomShow}
+                            <button onClick={this.randomToogle}>RandomChar Toogle</button>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col md='6'>
+                            <ItemList />
+                        </Col>
+                        <Col md='6'>
+                            <CharDetails />
+                        </Col>
+                    </Row>
+                </Container>
+            </>
+        )
+    };
+};
