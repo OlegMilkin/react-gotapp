9bbc580f68d2ead68666c35e91cb4ba30a6eab59
OlegMilkin <olegek.milkin@gmail.com>
2021-04-19 22:36:46 +0300

    home work

---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './spinner.css'
+
+const Spinner = () => {
+    return (
+        <div className="loadingio-spinner-spinner-58f37wo4ejq"><div className="ldio-9xcmhculet">
+            <div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
+        </div>
+    )
+}
+
+export default Spinner;
